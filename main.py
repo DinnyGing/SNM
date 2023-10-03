37ebda536e2e2d77c713f1cb7a10c22fbc55863a
Igor <135720728+IhorSikorskyi@users.noreply.github.com>
2023-10-03 11:16:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -67,3 +67,104 @@ class NeuralNetwork:
     def tanh_backward(dA, T):
         dT = tanh(T)
         return dA * (1 - np.square(dT))
+
+# Власне, поширення вперед. На 73 строці  - для огдного шару, а на 87 для всієї ШНМ.
+
+    def single_layer_forward_propagation(A_prev, W_curr, b_curr, activation="relu"):
+        S_curr = np.dot(W_curr, A_prev) + b_curr
+
+        if activation is "relu":
+            activation_func = relu
+        elif activation is "sigmoid":
+            activation_func = sigmoid
+        elif activation is "tanh":
+            activation_func = tanh
+        else:
+            raise Exception('Non-supported activation function')
+
+        return activation_func(S_curr), S_curr
+
+    def full_forward_propagation(X, params_values, nn_architecture):
+        memory = {}
+        A_curr = X
+
+        for idx, layer in enumerate(nn_architecture):
+            layer_idx = idx + 1
+            A_prev = A_curr
+
+            activ_function_curr = layer["activation"]
+            W_curr = params_values["W" + str(layer_idx)]
+            b_curr = params_values["b" + str(layer_idx)]
+            A_curr, Z_curr = single_layer_forward_propagation(A_prev, W_curr, b_curr, activ_function_curr)
+
+            memory["A" + str(idx)] = A_prev
+            memory["Z" + str(layer_idx)] = Z_curr
+
+        return A_curr, memory
+
+# Це вже функції зворотнього поширення для градієнту. Розділені так само,
+    # як і прямого поширення на 106 та 125 строках відповідно
+    def single_layer_backward_propagation(dA_curr, W_curr, b_curr, Z_curr, A_prev, activation="relu"):
+        m = A_prev.shape[1]
+
+        if activation is "relu":
+            backward_activation_func = relu_backward
+        elif activation is "sigmoid":
+            backward_activation_func = sigmoid_backward
+        elif activation is "tanh":
+            backward_activation_fun = tanh_backward
+        else:
+            raise Exception('Non-supported activation function')
+
+        dZ_curr = backward_activation_func(dA_curr, Z_curr)
+        dW_curr = np.dot(dZ_curr, A_prev.T) / m
+        db_curr = np.sum(dZ_curr, axis=1, keepdims=True) / m
+        dA_prev = np.dot(W_curr.T, dZ_curr)
+
+        return dA_prev, dW_curr, db_curr
+
+    def full_backward_propagation(Y_hat, Y, memory, params_values, nn_architecture):
+        grads_values = {}
+        m = Y.shape[1]
+        Y = Y.reshape(Y_hat.shape)
+
+        dA_prev = - (np.divide(Y, Y_hat) - np.divide(1 - Y, 1 - Y_hat));
+
+        for layer_idx_prev, layer in reversed(list(enumerate(nn_architecture))):
+            layer_idx_curr = layer_idx_prev + 1
+            activ_function_curr = layer["activation"]
+
+            dA_curr = dA_prev
+
+            A_prev = memory["A" + str(layer_idx_prev)]
+            Z_curr = memory["Z" + str(layer_idx_curr)]
+            W_curr = params_values["W" + str(layer_idx_curr)]
+            b_curr = params_values["b" + str(layer_idx_curr)]
+
+            dA_prev, dW_curr, db_curr = single_layer_backward_propagation(
+                dA_curr, W_curr, b_curr, Z_curr, A_prev, activ_function_curr)
+
+            grads_values["dW" + str(layer_idx_curr)] = dW_curr
+            grads_values["db" + str(layer_idx_curr)] = db_curr
+
+        return grads_values
+
+
+# оновлення значень параметрів, з двома масива для поточних значень параметрів
+# та похідні функції витрат відповідно
+def update(params_values, grads_values, nn_architecture, learning_rate):
+    for layer_idx, layer in enumerate(nn_architecture):
+        params_values["W" + str(layer_idx)] -= learning_rate * grads_values["dW" + str(layer_idx)]
+        params_values["b" + str(layer_idx)] -= learning_rate * grads_values["db" + str(layer_idx)]
+
+    return params_values;
+
+# функція втрат. Ця, можливо, не підійде, тоді просто видали/закоментуй. Бінарну кроссентропія
+def get_cost_value(Y_hat, Y):
+    m = Y_hat.shape[1]
+    cost = -1 / m * (np.dot(Y, np.log(Y_hat).T) + np.dot(1 - Y, np.log(1 - Y_hat).T))
+    return np.squeeze(cost)
+
+def get_accuracy_value(Y_hat, Y):
+    Y_hat_ = convert_prob_into_class(Y_hat)
+    return (Y_hat_ == Y).all(axis=0).mean()

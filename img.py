6131017c33c042f47c39df343b82f4c21cc93f88
Dinny <di.zybina@gmail.com>
2023-10-15 22:33:17 +0300

    update

---
+++ img.py
@@ -0,0 +1,217 @@
+import idx2numpy
+import numpy as np
+
+def mix_and_separate_data(data):
+    n = data.shape[0]
+    mixed_data = data.sample(n)
+    n_train = round(n * 0.7)
+    train_data = mixed_data.iloc[:n_train]
+    test_data = mixed_data.iloc[n_train:n]
+    return {'train': train_data, 'test': test_data}
+
+
+def assign_class_number(data, column):
+    unique_values = data[column].unique()
+    char_to_number = {}
+    for i, char in enumerate(unique_values):
+        char_to_number[char] = i
+    data[column] = data[column].apply(lambda x: char_to_number[x])
+    return data
+
+
+def replace_na_value(data, column):
+    average_value = data[column].mean(skipna=True)
+    data[column].fillna(value=average_value, inplace=True)
+    return data
+
+
+def sigmoid(s):
+    return 1 / (1 + np.exp(-s))
+
+
+def sigmoid_backward(s):
+    sig = sigmoid(s)
+    return sig * (1 - sig)
+
+
+def relu(x):
+    x[x <= 0] = 0
+    return x
+
+
+def relu_backward(x):
+    x[x > 0] = 1
+    x[x <= 0] = 0
+    return x
+
+
+def tanh(t):
+    return np.tanh(t)
+
+
+def tanh_backward(t):
+    dT = tanh(t)
+    return 1 - np.square(dT)
+
+
+def categorical_crossentropy(y_true, y_pred):
+    epsilon = 1e-15
+    y = np.maximum(epsilon, np.minimum(1 - epsilon, y_true))  # Обмежимо значення y між epsilon та 1-epsilon
+    return -np.mean(np.sum(y * np.log(y_pred), axis=1))
+
+
+def predict(inputs):
+    inputs_1 = np.dot(weights_0_1, inputs)
+    for i in range(len(inputs_1)):
+        inputs_1[i] += bias_0_1[i]
+    outputs_1 = fun_l1(inputs_1)
+
+    inputs_2 = np.dot(weights_1_2, outputs_1)
+    for i in range(len(inputs_2)):
+        inputs_2[i] += bias_1_2[i]
+    outputs_2 = fun_l2(inputs_2)
+
+    return outputs_2
+
+def forward_propagation(inputs):
+    inputs_1 = np.dot(weights_0_1, inputs) + bias_0_1
+    outputs_1 = fun_l1(inputs_1)
+
+    inputs_2 = np.dot(weights_1_2, outputs_1) + bias_1_2
+    outputs_2 = fun_l2(inputs_2)
+    return outputs_1, outputs_2
+
+
+def back_propagation(outputs_1, outputs_2, inputs, expected_predict):
+    error_layer_2 = np.array([outputs_2 - expected_predict])
+    gradient_layer_2 = fun_back_l2(outputs_2)
+    weights_delta_layer_2 = error_layer_2 * gradient_layer_2
+    calc_weights_1_2 = weights_1_2 - learning_rate * (outputs_1.reshape(len(outputs_1), 1) @ weights_delta_layer_2).T
+    calc_bias_1_2 = bias_1_2 - learning_rate * weights_delta_layer_2
+
+    error_layer_1 = weights_delta_layer_2.dot(weights_1_2)
+    gradient_layer_1 = fun_back_l1(outputs_1)
+    weights_delta_layer_1 = error_layer_1 * gradient_layer_1
+    calc_weights_0_1 = weights_0_1 - learning_rate * (inputs.reshape(len(inputs), 1) @ weights_delta_layer_1).T
+    calc_bias_0_1 = bias_0_1 - learning_rate * weights_delta_layer_1
+
+    return calc_weights_0_1, calc_weights_1_2, \
+        calc_bias_0_1[0], calc_bias_1_2[0]
+
+def SGD(X, y):
+    calc_weights_1_2 = weights_1_2
+    calc_weights_0_1 = weights_0_1
+    calc_bias_1_2 = bias_1_2
+    calc_bias_0_1 = bias_0_1
+    for i in range(0, len(X), batch_size):
+        # Forward pass
+        batch_input = X[i:i+batch_size].reshape(-1, input_layer_count)
+        batch_labels = y[i:i+batch_size]
+
+        inputs_1 = np.dot(batch_input, weights_0_1.T)
+        for i in range(len(inputs_1.T)):
+            inputs_1.T[i] += bias_0_1[i]
+        outputs_1 = fun_l1(inputs_1)
+
+        inputs_2 = np.dot(outputs_1, weights_1_2.T)
+        for i in range(len(inputs_2.T)):
+            inputs_2.T[i] += bias_1_2[i]
+        outputs_2 = fun_l2(inputs_2)
+
+        # Calculate the error
+        batch_labels_expected = []
+        for i in range(len(batch_labels)):
+            batch_labels_expected.append(expect(batch_labels))
+        batch_labels_expected = np.array(batch_labels_expected)
+        error_layer_2 = outputs_2 - batch_labels_expected
+        weights_delta_layer_2 = error_layer_2 * fun_back_l2(outputs_2)
+        calc_weights_1_2 = weights_1_2 - learning_rate * (outputs_1 @ weights_delta_layer_2).T
+        calc_bias_1_2 = bias_1_2 - learning_rate * weights_delta_layer_2
+
+        error_layer_1 = weights_delta_layer_2.dot(weights_1_2)
+        weights_delta_layer_1 = error_layer_1 * fun_back_l1(outputs_1)
+        calc_weights_0_1 = weights_0_1 - learning_rate * (batch_input @ weights_delta_layer_1).T
+        calc_bias_0_1 = bias_0_1 - learning_rate * weights_delta_layer_1
+    return calc_weights_0_1, calc_weights_1_2, \
+        calc_bias_0_1[0], calc_bias_1_2[0]
+
+def train(inputs, expected_predict):
+    outputs_1, outputs_2 = forward_propagation(inputs)
+    return back_propagation(outputs_1, outputs_2, inputs, expected_predict)
+
+
+def test(inputs, outputs):
+    accuracy = 0
+    for index in range(len(inputs)):
+        labels = inputs[index].reshape(1, -1)[0]
+        actual = predict(np.array(labels))
+        pos = outputs[index]
+        for i in range(len(actual)):
+            if actual[i] >= actual[pos]:
+                pos = i
+        if pos == outputs[index]:
+            accuracy += 1
+    return accuracy / len(outputs)
+
+
+def expect(index):
+    expected = np.zeros(output_layer_count)
+    expected[index] = 1
+    return expected
+
+
+def get_attributes(columns, target):
+    attributes = []
+    for column in columns:
+        if column != target:
+            attributes.append(column)
+    return attributes
+
+train_data = np.array(idx2numpy.convert_from_file("train-images.idx3-ubyte"))
+train_labels = np.array(idx2numpy.convert_from_file("train-labels.idx1-ubyte"))
+test_data = np.array(idx2numpy.convert_from_file("t10k-images.idx3-ubyte"))
+test_labels = np.array(idx2numpy.convert_from_file("t10k-labels.idx1-ubyte"))
+
+hide_layer_count = 128
+input_layer_count = len(train_data[0]) * len(test_data[0][0])
+output_layer_count = 10
+weights_0_1 = np.random.normal(-0.5, 0.5, (hide_layer_count, input_layer_count))
+weights_1_2 = np.random.normal(-0.5, 0.5 ** -0.5, (output_layer_count, hide_layer_count))
+
+bias_0_1 = np.zeros(hide_layer_count)
+bias_1_2 = np.zeros(output_layer_count)
+
+epochs = 5
+learning_rate = 0.07
+batch_size = 32
+theta = None
+q1 = 0
+q2 = 0
+fun_l1 = tanh
+fun_l2 = sigmoid
+fun_back_l1 = tanh_backward
+fun_back_l2 = sigmoid_backward
+train_data = train_data.reshape(len(train_data), input_layer_count)
+
+for e in range(epochs):
+    inputs_ = []
+    correct_predictions = []
+    for index in range(len(train_data)):
+        labels = train_data[index].reshape(1, -1)[0]
+        expected = expect(train_labels[index])
+        # weights_0_1, weights_1_2, \
+        #     bias_0_1, bias_1_2 = train(np.array(labels), expected)
+        inputs_.append(np.array(labels))
+        correct_predictions.append(np.array(expected))
+    weights_0_1, weights_1_2, \
+        bias_0_1, bias_1_2 = SGD(np.array(train_data), train_labels)
+    train_loss = categorical_crossentropy(np.array(correct_predictions),
+                                          predict(np.array(inputs_).T).T)
+
+    progress = 100 * e/float(epochs)
+    if progress % 10 == 0:
+        progress = str(progress)[:4]
+        print("\rProgress: " + progress + " Training loss: ")
+        print(train_loss)
+
+print("Accuracy: " + str(test(test_data, test_labels)))
